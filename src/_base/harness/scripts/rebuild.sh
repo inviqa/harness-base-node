4680c7b3967c67e11a6daaea57fbfa061751ea4a
Marco Massari Calderone <marco@marcomc.com>
2021-06-09 17:17:40 -0300

    restore docker-compose timeout to 120 secs

--- src/_base/harness/scripts/rebuild.sh
+++ src/_base/harness/scripts/rebuild.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-run docker-compose down --rmi local --volumes --remove-orphans --timeout 12
+run docker-compose down --rmi local --volumes --remove-orphans --timeout 120
 
 passthru ws cleanup built-images
 
